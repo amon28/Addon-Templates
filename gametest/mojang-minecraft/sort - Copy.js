bb8dd60d01a6bff66f876609e7ed118302457a69
amon28 <richmondgerez@gmail.com>
2021-10-23 07:11:35 +0800

    Add files via upload

---
+++ gametest/mojang-minecraft/sort - Copy.js
@@ -0,0 +1,26 @@
+import * as GameTest from 'mojang-gametest';
+import {Commands,World,ItemStack,MinecraftItemTypes} from 'mojang-minecraft';
+    
+const chatCallback = World.events.beforeChat;
+
+chatCallback.subscribe(bChat);	  
+ 
+function bChat(chatData) 
+{
+if(chatData.message == "!inv"){
+try{
+let player = chatData.sender;
+let pInv = player.getComponent("inventory").container;
+	for(let slot=0; slot<pInv.size; slot++){
+		let item = pInv.getItem(slot);
+		if(item != null){
+		let iName = item.id;
+		Commands.run(`say ${iName}`, World.getDimension("overworld"));		
+		}
+	}
+	
+}catch(e){
+Commands.run(`say ${e}`, World.getDimension("overworld"));			
+}
+}
+}	
