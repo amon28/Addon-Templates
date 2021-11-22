920630597c43b9d6de1ce27878500bbe82b46020
amon28 <richmondgerez@gmail.com>
2021-11-22 17:23:09 +0800

    updated to 1.18

--- gametest/mojang-minecraft/check_for_specific_item_in_inventory.js
+++ gametest/mojang-minecraft/check_for_specific_item_in_inventory.js
@@ -1,6 +1,6 @@
-import {Commands,World,ItemStack,MinecraftItemTypes} from 'mojang-minecraft';
+import {world,ItemStack,MinecraftItemTypes} from 'mojang-minecraft';
     
-const chatCallback = World.events.beforeChat;
+const chatCallback = world.events.beforeChat;
 
 chatCallback.subscribe(bChat);	  
  
@@ -8,23 +8,20 @@ function bChat(chatData)
 {
 if(chatData.message == "!inv"){
 try{
-let apple = 0;
 let player = chatData.sender;
 let pInv = player.getComponent("inventory").container;
 	for(let slot=0; slot<pInv.size; slot++){
 		let item = pInv.getItem(slot);
-		if(item != null){
+		if(!item) continue;
 		if(item.id == "minecraft:apple"){
-		apple++;		
+		player.runCommand(`say Apple Found!`);
+		break;
 		}
-		}
-	}
-	if(apple>=1){
-	Commands.run(`say Apple Found!`, World.getDimension("overworld"));		
+		
 	}
 	
 }catch(e){
-Commands.run(`say ${e}`, World.getDimension("overworld"));			
+player.runCommand(`say ${e}`);			
 }
 }
 }	
