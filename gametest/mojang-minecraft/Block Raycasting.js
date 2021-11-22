bb0ec552f084327907d13c67561fcfc1deb529a5
amon28 <richmondgerez@gmail.com>
2021-11-22 16:56:21 +0800

    Create Block Raycasting.js

---
+++ gametest/mojang-minecraft/Block Raycasting.js
@@ -0,0 +1,25 @@
+import { world, BlockRaycastOptions, Block, MinecraftBlockTypes } from 'mojang-minecraft';
+
+const eventTick = world.events.tick;
+eventTick.subscribe(onTick);
+
+
+function onTick(eventData){
+
+let players = world.getPlayers();
+	for(var index in players){
+		let player = players[index];
+		let blockRayCast = new BlockRaycastOptions();
+		blockRayCast.includeLiquidBlocks = false;
+		blockRayCast.includePassableBlocks = true;
+		blockRayCast.maxDistance = 9;
+		let block = player.getBlockFromViewVector(blockRayCast);
+		let currentDimension = players[index].dimension;
+		//do something
+		if(block != null){
+		currentDimension.runCommand(`title @p[name="${player.nameTag}"] actionbar ${block.type.id}`);		
+		}else{
+		currentDimension.runCommand(`title @p[name="${player.nameTag}"] clear`);	
+		}
+	}
+}
