a3431e7ea151751f6465bffc593fa38fba76a0a7
amon28 <richmondgerez@gmail.com>
2021-11-22 17:16:11 +0800

    Update currentDimension.js

--- gametest/mojang-minecraft/snippets/currentDimension.js
+++ gametest/mojang-minecraft/snippets/currentDimension.js
@@ -1,7 +1,8 @@
+//In 1.18 you can use entity.dimension or player.dimension to get the current dimension
 function currentDimension(x,y,z){
-let overworld = World.getDimension("overworld").getEntitiesAtBlockLocation(new BlockLocation(x,y,z));	
-let nether = World.getDimension("nether").getEntitiesAtBlockLocation(new BlockLocation(x,y,z));
-let theEnd = World.getDimension("the end").getEntitiesAtBlockLocation(new BlockLocation(x,y,z));
+let overworld = world.getDimension("overworld").getEntitiesAtBlockLocation(new BlockLocation(x,y,z));	
+let nether = world.getDimension("nether").getEntitiesAtBlockLocation(new BlockLocation(x,y,z));
+let theEnd = world.getDimension("the end").getEntitiesAtBlockLocation(new BlockLocation(x,y,z));
 
 for(let count=0; count<overworld.length; count++){
 	if(overworld[count].id == "minecraft:player"){
