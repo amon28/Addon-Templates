83c988e67d25155b1a6314e27b48e76c6637c36f
amon28 <richmondgerez@gmail.com>
2021-12-02 20:40:21 +0800

    For 1.18.0 stable
    changed for() to find()

--- gametest/mojang-minecraft/snippets/currentDimension.js
+++ gametest/mojang-minecraft/snippets/currentDimension.js
@@ -5,19 +5,18 @@ let overworld = World.getDimension("overworld").getEntitiesAtBlockLocation(new B
 let nether = World.getDimension("nether").getEntitiesAtBlockLocation(new BlockLocation(x,y,z));
 let theEnd = World.getDimension("the end").getEntitiesAtBlockLocation(new BlockLocation(x,y,z));
 
-for(let count=0; count<overworld.length; count++){
-	if(overworld[count].id == "minecraft:player"){
+	if(overworld.find(p => p.id == "minecraft:player")){
 	return "overworld";
 	}
-}
-for(let count=0; count<nether.length; count++){
-	if(nether[count].id == "minecraft:player"){
+
+
+	if(nether.find(p => p.id == "minecraft:player")){
 	return "nether";
 	}
-}
-for(let count=0; count<theEnd.length; count++){
-	if(theEnd[count].id == "minecraft:player"){
+
+	if(theEnd.find(p => p.id == "minecraft:player")){
 	return "the end";
 	}
-}	
+
+	Console.warn(`No Dimension Detected`);
 }
