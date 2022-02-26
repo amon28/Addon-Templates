4b6501e2be20c21eea2fa779dce97d33196d3db5
amon28 <richmondgerez@gmail.com>
2022-02-26 21:37:56 +0800

    Added comments and fixed signs

--- gametest/mojang-minecraft/snippets/blockToItemStack.js
+++ gametest/mojang-minecraft/snippets/blockToItemStack.js
@@ -1,10 +1,18 @@
 function blockToItem(block){
 try{
 let blockId = block.id;
+//world.getDimension(`overworld`).runCommand(`say ${block.id}`);
+	//Change concrete powder to match item id
 	if(blockId.includes(`powder`)) blockId = 'minecraft:concrete_powder';
+	//Change slabs to match item id
 	if(blockId.includes(`stone_slab`) && !blockId.includes(`double_`)){
 		blockId = blockId.split(':')[0] + ':' + 'double_' + blockId.split(':')[1];	
 	}
+	//Change sign to match item id
+	if(blockId.includes(`_standing`)) blockId = blockId.replace(/_standing/g,'');
+	if(blockId==`minecraft:standing_sign`) blockId = "minecraft:oak_sign";
+	if(blockId.includes('dark') && blockId.includes('sign')) blockId = "minecraft:dark_oak_sign";
+	
 let currentValue = '';
 let validValues = [];
 let blockData;
@@ -14,7 +22,7 @@ let blockData;
 			let bProperty = block.permutation.getProperty(p[p2]);
 			currentValue = bProperty.value;
 			validValues = bProperty.validValues;
-			blockData = validValues.findIndex(value=>value==currentValue);
+			blockData = validValues.findIndex(value=>value==currentValue);	
 			let newItemStack = new ItemStack(Items.get(blockId),1,blockData);
 			return newItemStack;			
 			}else{
